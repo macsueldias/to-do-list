9489a2cabc753dfdcc6d8d49b33751554d4463ba
Macsuel Dias <66310986+macsueldias@users.noreply.github.com>
2020-08-01 22:23:04 -0300

    JS

--- Projeto/js/script.js
+++ Projeto/js/script.js
@@ -9,17 +9,73 @@ class Todo {
 
         // remove classe hide
         template.classList.remove('hide')
-        
+
         // manipular texto
         let templateText = template.querySelector('.task-title')
-        template.textContent = taskText
+        templateText.textContent = taskText
 
         let list = document.querySelector('#tasks-container')
-        
+
         // inserir na lista
         list.appendChild(template)
+
+        // adiciona evento as tasks
+        this.addEvents()
+
+        this.checkTasks('add')
+    }
+    
+    removeTask(task) {
+        // achar o elemento
+        let parentE1 = task.parentElement
+
+        // remover da lista
+        parentE1.remove()
+
+        this.checkTasks('remove')
+
     }
 
+    completeTask(task) {
+
+        // adiciona a classe de done
+        task.classList.add('done')
+    }
+
+    addEvents() {
+        let removeBtns = document.querySelectorAll('.fa-trash')
+        let removeBtn = removeBtns[removeBtns.length - 1]
+        let doneBtns = document.querySelectorAll('.fa-check')
+        let doneBtn = doneBtns[doneBtns.length - 1]
+
+        // adicionar evento de remover
+        removeBtn.addEventListener('click', function() {
+            todo.removeTask(this)
+        })
+
+        // adicionar evento de completar tarefa
+        doneBtn.addEventListener('click', function() {
+            todo.completeTask(this)
+        })
+    }
+
+    checkTasks(comand) {
+        let msg = document.querySelector('#empty-tasks')
+
+        // logica de adicionar ou remover tasks
+        if(comand === 'add') {
+            this.totalTasks += 1
+        } else if(comand === 'remove') {
+            this.totalTasks -= 1
+        }
+
+        // checa se tem mais de uma task e adiciona ou remove a classe
+        if(this.totalTasks == 1) {
+            msg.classList.remove('hide')
+        } else {
+            msg.classList.add('hide')
+        }
+    }
 
 }
 
@@ -28,14 +84,14 @@ let todo = new Todo()
 // events
 let addBtn = document.querySelector('#add')
 
-addBtn.addEventListener('click', function(e) {
-    
+addBtn.addEventListener('click', function (e) {
+
     e.preventDefault()
 
     let taskText = document.querySelector('#task')
 
-    if(taskText != ''){
-    todo.addTask(taskText.value)
+    if (taskText.value != '') {
+        todo.addTask(taskText.value)
     }
 
     // limpa campo de texto
